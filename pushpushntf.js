943d1027f5cab2815417f39e753331c5d46fe92e
ps77ofc <127024012+ps77ofc@users.noreply.github.com>
2025-05-05 16:09:42 +0800

    Create pushpushntf.js

---
+++ pushpushntf.js
@@ -0,0 +1,9 @@
+if(location.hostname == "playslot77ultra.com"){
+             (function(d, t) {
+                var g = d.createElement(t),
+                s = d.getElementsByTagName(t)[0];
+                g.src = "https://cdn.pushalert.co/integrate_a17ccb07007eb796166dbb59c034cd7b.js";
+                s.parentNode.insertBefore(g, s);
+        }(document, "script"));
+    }
+    </script>
